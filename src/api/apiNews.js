93c5f9b9c948694f7ef6806905a6d410f285c9b6
Mykhail <gorgefraggggg@gmail.com>
2024-07-16 21:22:11 +0300

    change API

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 
 export const getNews = async () => {
   try {
-    const response = await axios.get('/api/latest-news', {
+    const response = await axios.get(`${BASE_URL}/latest-news`, {
       params: {
         apiKey: API_KEY,
       },
@@ -13,6 +14,7 @@ export const getNews = async () => {
       },
     });
 
+    console.log('API response:', response.data); // Добавлено для отладки
     return response.data;
   } catch (error) {
     console.error('Error fetching news:', error);
